7ae46bcd525a742769a2167bc2fff65ade5d2d2e
Andrew Dymond <andy.dymond@gmail.com>
2018-02-10 16:33:06 -0700

    Add spec to test default user can access admin index

--- spec/features/users/admin_sees_recipes_index_spec.rb
+++ spec/features/users/admin_sees_recipes_index_spec.rb
@@ -11,4 +11,15 @@ describe "User visits recipes index" do
       expect(page).to have_content("All Chef Recipes")
     end
   end
+  context "as default user" do
+    it "doesn't allow default user to view all recipes" do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_recipes_path
+      expect(page).to_not have_content("All Chef Recipes")
+      expect(page).to have_content("The page you are looking for does not exist.")
+    end
+  end
 end
