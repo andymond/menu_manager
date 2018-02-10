a268c3a497b27b9eb12197656601b0237db58f86
Andrew Dymond <andy.dymond@gmail.com>
2018-02-10 12:34:11 -0700

    Add user sees all recipes spec

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,7 +4,7 @@ describe Recipe, type: :model do
   describe "Validations" do
     describe "valid attributes" do
       it "is valid with name, instructions, and status" do
-        recipe = Recipe.create(name: "Coconut Rice", instructions: "Adjust water amount to the rice age: the newer rice needs less water. Combine water, coconut cream, sugar and salt and mix well until sugar and salt are disolved. Combine with completely rinsed and drained rice, mix thoroughly in cooker, making sure rice is level when finished mixing. Turn cooker on. When cook phase has finished and 'warm' light is on, allow rice to rest 20-30 minutes, then fluff lightly and transfer to rice warmer.", status: "complete" )
+        recipe = create(:recipe)
 
         expect(recipe).to be_valid
       end
