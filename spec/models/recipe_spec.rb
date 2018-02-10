d57071745b80f09735ac02a7a4dce92de14ed213
Andrew Dymond <andy.dymond@gmail.com>
2018-02-10 11:50:13 -0700

    Recipe Validations Spec passing

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,10 +2,30 @@ require "rails_helper"
 
 describe Recipe, type: :model do
   describe "Validations" do
-    it "is valid with name, instructions, and status" do
-      recipe = Recipe.create(name: "Coconut Rice", instructions: "Adjust water amount to the rice age: the newer rice needs less water. Combine water, coconut cream, sugar and salt and mix well until sugar and salt are disolved. Combine with completely rinsed and drained rice, mix thoroughly in cooker, making sure rice is level when finished mixing. Turn cooker on. When cook phase has finished and 'warm' light is on, allow rice to rest 20-30 minutes, then fluff lightly and transfer to rice warmer.", status: "complete" )
+    describe "valid attributes" do
+      it "is valid with name, instructions, and status" do
+        recipe = Recipe.create(name: "Coconut Rice", instructions: "Adjust water amount to the rice age: the newer rice needs less water. Combine water, coconut cream, sugar and salt and mix well until sugar and salt are disolved. Combine with completely rinsed and drained rice, mix thoroughly in cooker, making sure rice is level when finished mixing. Turn cooker on. When cook phase has finished and 'warm' light is on, allow rice to rest 20-30 minutes, then fluff lightly and transfer to rice warmer.", status: "complete" )
 
-      expect(recipe).to be_valid
+        expect(recipe).to be_valid
+      end
+    end
+
+    describe "invalid attributes" do
+      it "is invalid without name" do
+        recipe = Recipe.create(instructions: "Adjust water amount to the rice age: the newer rice needs less water. Combine water, coconut cream, sugar and salt and mix well until sugar and salt are disolved. Combine with completely rinsed and drained rice, mix thoroughly in cooker, making sure rice is level when finished mixing. Turn cooker on. When cook phase has finished and 'warm' light is on, allow rice to rest 20-30 minutes, then fluff lightly and transfer to rice warmer.", status: "complete" )
+
+        expect(recipe).to be_invalid
+      end
+      it "is invalid without instructions" do
+        recipe = Recipe.create(name: "Coconut Rice", status: "complete" )
+
+        expect(recipe).to be_invalid
+      end
+      it "is invalid without status" do
+        recipe = Recipe.create(name: "Coconut Rice", instructions: "Adjust water amount to the rice age: the newer rice needs less water. Combine water, coconut cream, sugar and salt and mix well until sugar and salt are disolved. Combine with completely rinsed and drained rice, mix thoroughly in cooker, making sure rice is level when finished mixing. Turn cooker on. When cook phase has finished and 'warm' light is on, allow rice to rest 20-30 minutes, then fluff lightly and transfer to rice warmer.")
+
+        expect(recipe).to be_invalid
+      end
     end
   end
 end
