1381411a92486b95923247081a8ce3dce22e4342
Andrew Dymond <andy.dymond@gmail.com>
2018-02-11 12:59:28 -0700

    Add user info to nav bar for troubleshooting

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -4,8 +4,19 @@ class Admin::RecipesController < Admin::BaseController
   end
 
   def edit
-    @admin = current_admin
     @recipe = Recipe.find(params[:id])
     @users = User.all
+  end
+
+  def update
+    @recipe = Recipe.update(recipe_params)
+    flash.notice = "#{@recipe.name} updated."
+    redirect_to admin_recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:user_id, :name, :instructions, :status, :image)
   end
 end
