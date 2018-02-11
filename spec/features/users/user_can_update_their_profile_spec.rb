01c6a584fc46806e5e9307a6cc1d7f7900f01f46
Andrew Dymond <andy.dymond@gmail.com>
2018-02-11 15:58:42 -0700

    Add user can't edit other users profiles spec

--- spec/features/users/user_can_update_their_profile_spec.rb
+++ spec/features/users/user_can_update_their_profile_spec.rb
@@ -15,4 +15,16 @@ describe "default user can update their own profile" do
     expect(page).to have_content("hushpuppies666@hotmail.net")
     expect(page).to have_content("User info updated.")
  end
+ it "doesn't allow them to update others' profiles" do
+   user = create(:user)
+   user_2 = create(:user)
+   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+   visit user_path(user_2)
+   expect(page).to_not have_link("Update User")
+
+   visit edit_user_path(user_2)
+
+   expect(page).to have_content("page doesn't exist")
+ end
 end
