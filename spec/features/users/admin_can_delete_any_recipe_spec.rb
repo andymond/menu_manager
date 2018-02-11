a8f5f95fc20da197cb7bb453b29d85f49aec850a
Andrew Dymond <andy.dymond@gmail.com>
2018-02-11 11:32:55 -0700

    Add admin can delete any recipe spec

---
+++ spec/features/users/admin_can_delete_any_recipe_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+describe "admin can delete any recipe"
+  context "as admin" do
+    it "allows admin to delete any recipe" do
+      admin = create(:user, role: 1)
+      recipe_1 = create(:recipe, user_id: admin.id)
+      recipe_2 = create(:recipe, user_id: admin.id, status: "complete")
+      recipe_3 = create(:recipe, user_id: admin.id, status: "in_progress")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit recipe_path(recipe_1)
+      click_on "Delete recipe_1"
+
+      expect(current_path).to eq(recipes_path)
+      expect(page).to have_content("recipe_1 was deleted")
+      expect(page).to_not have_content(recipe_1)
+
+      visit recipe_path(recipe_2)
+      click_on "Delete recipe_2"
+
+      expect(current_path).to eq(recipes_path)
+      expect(page).to have_content("recipe_2 was deleted")
+      expect(page).to_not have_content(recipe_2)
+
+      visit recipe_path(recipe_3)
+      click_on "Delete recipe_3"
+
+      expect(current_path).to eq(recipes_path)
+      expect(page).to have_content("recipe_3 was deleted")
+      expect(page).to_not have_content(recipe_3)
+    end
+  end
