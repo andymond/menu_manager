d51c5287e221ef73917cd6627dc18b713584ebe2
Andrew Dymond <andy.dymond@gmail.com>
2018-02-11 18:08:04 -0700

    Add recipe_category_spec to model specs

---
+++ spec/models/recipe_category_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe RecipeCategory, type: :model do
+  describe "validations" do
+    it "is valid with recipe_id & category_id" do
+      recipe = create(:recipe)
+      category = create(:category)
+      rc = RecipeCategory.create(recipe_id: recipe.id, category_id: category.id)
+
+      expect(rc).to be_valid
+    end
+    it "is invalid without recipe_id " do
+      recipe = create(:recipe)
+      rc = RecipeCategory.create(recipe_id: recipe.id)
+
+      expect(rc).to be_invalid
+    end
+    it "is invalid without category_id" do
+      category = create(:category)
+      rc = RecipeCategory.create(category_id: category.id)
+
+      expect(rc).to be_invalid
+    end
+  end
+end
