d5877c036c803ca8c156308104c5fe267312eb62
Andrew Dymond <andy.dymond@gmail.com>
2018-02-11 17:13:15 -0700

    Add admins can delete users spec

---
+++ spec/features/users/admin/admins_can_delete_users_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe "admins can delete users" do
+  it "allows admin to delete user" do
+    user = create(:user)
+    admin = create(:user, role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit users_path
+    click_on "Remove #{user.username}"
+
+    expect(current_path).to eq(users_path)
+    expect(page).to_not have_content(user.username)
+  end
+end
