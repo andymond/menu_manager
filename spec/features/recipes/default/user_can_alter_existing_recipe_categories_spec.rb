b121660b7c640148f7b6101b1fbcf850fa882050
Andrew Dymond <andy.dymond@gmail.com>
2018-02-12 18:18:03 -0700

    Add user can add new category to recipe spec

--- spec/features/recipes/default/user_can_alter_existing_recipe_categories_spec.rb
+++ spec/features/recipes/default/user_can_alter_existing_recipe_categories_spec.rb
@@ -9,10 +9,20 @@ describe "user can alter existing recipes categories" do
 
     visit recipe_path(recipe)
     select(category.name, :from => "recipe[category_ids][]")
-    click_on("Set Recipe Categories")
+    click_on "Set Recipe Categories"
+
+    expect(page).to have_content("#{recipe.name} updated.")
+    expect(page).to have_link(category.name)
   end
-
   it "allows user to add new category" do
+    user = create(:user)
+    recipe = create(:recipe, user_id: user.id)
 
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit recipe_path(recipe)
+    click_on "New Category"
+
+    expect(current_path).to eq(new_category_path)
   end
 end
