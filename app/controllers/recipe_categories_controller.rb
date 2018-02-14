f5fd7eb0bfd39b0d05584653dd9be1a1705fe090
Andrew Dymond <andy.dymond@gmail.com>
2018-02-13 20:11:46 -0700

    Add flashes for recipe category creation

--- app/controllers/recipe_categories_controller.rb
+++ app/controllers/recipe_categories_controller.rb
@@ -5,11 +5,12 @@ class RecipeCategoriesController < ApplicationController
   end
 
   def create
-    binding.pry
     @rc = RecipeCategory.find_or_create_by(rc_params)
     if @rc.save
+      flash.notice = "Recipe Categories updated."
       redirect_to recipe_path(params[:recipe_id])
     else
+      flash.notice = "Failed to update Recipe Categories."
       render :new
     end
   end
