d62fc5d3c5e556c28953ba175e8755685a627dcc
Andrew Dymond <andy.dymond@gmail.com>
2018-02-14 18:53:29 -0700

    Recipes display their comments on their show page

--- spec/features/recipes/default/user_sees_one_recipe_spec.rb
+++ spec/features/recipes/default/user_sees_one_recipe_spec.rb
@@ -17,4 +17,17 @@ describe "user navigates to recipe show page" do
     expect(page).to have_content(recipe.instructions)
     expect(page).to have_content(recipe.status)
   end
+  it "displays a comment option" do
+    recipe = create(:recipe, user_id: @user.id)
+    comment = recipe.comments.create(user: "bob", body: "nice!")
+    comment_2 = recipe.comments.create(user: "bot", body: "not!")
+
+
+    visit recipe_path(recipe)
+    
+    expect(page).to have_content(comment.user)
+    expect(page).to have_content(comment.body)
+    expect(page).to have_content(comment_2.user)
+    expect(page).to have_content(comment_2.body)
+  end
 end
