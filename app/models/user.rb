84760983a07452562b16dc00fe8c6e34955d97e7
Andrew Dymond <andy.dymond@gmail.com>
2018-02-15 18:43:34 -0700

    Add default image to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :email, :username, presence: true, uniqueness: true
   has_secure_password
   has_many :recipes, dependent: :destroy
-  has_attached_file :image
+  has_attached_file :image, default_url: 'naruto.svg'
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   enum role: [:default, :admin]
   scope :abc, -> {order(:username)}
